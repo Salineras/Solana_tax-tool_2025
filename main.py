a368767ba27c940d0837ccbaf427c20ab9d6c936
Salineras <a_erosmith@hotmail.com>
2025-09-20 14:00:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from modules import initial, analyzer, final
 
 def main():
     tx_lists, url = initial.fetch_tx()
-    all_txs       = initial.fetch_tx_data(tx_lists, url)  #本番ではTx取得失敗をsys.exitへ変更
+    all_txs       = initial.fetch_tx_data(tx_lists, url)
 
     df = initial.extract_tx(all_txs)
     df = initial.filter_scam(df)
@@ -15,5 +15,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# analyzerの最後部のPOISONIG と def analyze(df)からpoisoningの判定を削除すること！！！
