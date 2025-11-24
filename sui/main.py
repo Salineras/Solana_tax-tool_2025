62ac2807ec2c5bdb4f295551f7452267743d7593
Salineras <a_erosmith@hotmail.com>
2025-11-25 08:36:41 +0900

    Shinami採用

--- sui/main.py
+++ sui/main.py
@@ -1,12 +1,10 @@
 from resources import initial, analyzer, final
-import json
+import json, asyncio
 
 def main():
-    # digests = fetch_digest(url, addresses)
-    # txs = asyncio.run(run_firehose(url, digests))
+    digests, url = initial.fetch_digest()
+    txs = asyncio.run(initial.fetch_tx(url, digests))
 
-    with open("sui\\resources\\TxData.json", "r", encoding="utf-8") as f:
-        txs = json.load(f)
     df = initial.extract_tx(txs)
     df = initial.JST_ticker(df)
     df = analyzer.analyze(df)
@@ -15,4 +13,5 @@ def main():
     final.output(df)
 
 if __name__ == "__main__":
+
     main()
